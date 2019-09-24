28863e39d651ef18ee6e9b2f179e98591fd67047
joshua richards <jermain119@gmail.com>
2019-09-24 19:04:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,7 +11,7 @@ def using_unshift( array, string)
 end
 
 def using_pop(great_hits_of_the_nineties)
-  @great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
+  @continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
   @great_hits_of_the_nineties.pop
 end
 
