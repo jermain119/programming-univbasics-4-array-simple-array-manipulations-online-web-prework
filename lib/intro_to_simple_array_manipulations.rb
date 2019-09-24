64e65750240856cc38889c02bea64ee38771c3a9
joshua richards <jermain119@gmail.com>
2019-09-24 18:57:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,11 +4,10 @@ def using_push(colors_in_the_rainbow ,  next_color  )
    colors_in_the_rainbow.push(next_color)
 end
 
-def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
+def using_unshift( bouroughs_in_nyc, new_neighborhood)
  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
-    @new_neighborhood = "Staten Island" = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
     @new_neighborhood = "Staten Island"
-  neighborhoods_in_northwest_brooklyn.unshift(@new_neighborhood)
+   bouroughs_in_nyc(@new_neighborhood)
 end
 
 def using_pop(great_hits_of_the_nineties)
