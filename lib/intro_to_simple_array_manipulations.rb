5d29fa9f2419daf1959abceadc9a98a6e93691fa
joshua richards <jermain119@gmail.com>
2019-09-24 19:19:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -37,8 +37,8 @@ def using_concat(my_favorite_things, more_favs)
 end
 
 def using_insert(list_of_esoteric_programming_languages, another_esoteric_language)
-  @list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
-  @another_esoteric_language = "Malbolge"
+  @list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+    @another_language = "Python"
   @new_array = @list_of_esoteric_programming_languages.insert(4, @another_esoteric_language)
 end
 
