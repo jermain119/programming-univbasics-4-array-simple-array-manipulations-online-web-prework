44d37f65b5870b8443278fa0a59b509f69e4131c
joshua richards <jermain119@gmail.com>
2019-09-24 19:01:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,10 +4,10 @@ def using_push(colors_in_the_rainbow ,  next_color  )
    colors_in_the_rainbow.push(next_color)
 end
 
-def using_unshift( bouroughs_in_nyc, new_neighborhood)
- bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+def using_unshift( array, string)
+ array = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
     string = "Staten Island"
-   bouroughs_in_nyc(string)
+   bouroughs_in_nyc.unshift(string)
 end
 
 def using_pop(great_hits_of_the_nineties)
